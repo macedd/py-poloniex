f0f478490254fd7c0760c382aa16b320acb3bcf0
Thiago Fernandes <thiago@internetbudi.com.br>
2016-11-02 18:00:35 -0200

    pypi release v3

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+[![PyPI version](https://badge.fury.io/py/py-poloniex.svg)](https://badge.fury.io/py/py-poloniex)
+[![Build Status](https://travis-ci.org/thiagof/py-poloniex.svg?branch=master)](https://travis-ci.org/thiagof/py-poloniex)
+
 Poloniex API
 ==========================
 
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 
 
 setuptools.setup(name='py-poloniex',
-                 version='0.2',
-                 description='Python Poloniex API Wrapper',
+                 version='0.3',
+                 description='Python Poloniex API',
                  long_description=open('README.md').read().strip(),
                  author='Thiago Fernandes Macedo',
                  author_email='thiago@internetbudi.com.br',
@@ -13,4 +13,26 @@ setuptools.setup(name='py-poloniex',
                  license='MIT License',
                  zip_safe=False,
                  keywords='poloniex bitcoin btc api exchange',
-                 classifiers=[])
+                 classifiers=[
+                    # How mature is this project? Common values are
+                    #   3 - Alpha
+                    #   4 - Beta
+                    #   5 - Production/Stable
+                    'Development Status :: 4 - Beta',
+
+                    # Indicate who your project is intended for
+                    'Intended Audience :: Developers',
+                    'Topic :: Software Development :: Bitcoin',
+
+                    # Pick your license as you wish (should match "license" above)
+                     'License :: OSI Approved :: MIT License',
+
+                    # Specify the Python versions you support here. In particular, ensure
+                    # that you indicate whether you support Python 2, Python 3 or both.
+                    'Programming Language :: Python :: 2.6',
+                    'Programming Language :: Python :: 2.7',
+                    'Programming Language :: Python :: 3.3',
+                    'Programming Language :: Python :: 3.4',
+                    'Programming Language :: Python :: 3.5',
+                ])
+
