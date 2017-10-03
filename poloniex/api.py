b74fa04743d00664696db1fd1e99d92b2a21018b
Thiago Macedo <thiago@internetbudi.com.br>
2017-10-03 20:33:16 -0300

    remove currencyPair from cancel order params

--- poloniex/api.py
+++ poloniex/api.py
@@ -1,6 +1,6 @@
-# 
+#
 # Based on http://pastebin.com/8fBVpjaj
-# 
+#
 
 try:
     # For Python 3.0 and later
@@ -32,10 +32,10 @@ class Poloniex:
         self.Secret = Secret
 
         self.parseJson = parseJson
- 
+
     def post_process(self, before):
         after = before
- 
+
         # Add timestamps if there isnt one but is a datetime
         if('return' in after):
             if(isinstance(after['return'], list)):
@@ -43,9 +43,9 @@ class Poloniex:
                     if(isinstance(after['return'][x], dict)):
                         if('datetime' in after['return'][x] and 'timestamp' not in after['return'][x]):
                             after['return'][x]['timestamp'] = float(createTimeStamp(after['return'][x]['datetime']))
-                           
+
         return after
- 
+
     def api(self, type, params):
         try:
             params = dict((k,v) for k,v in params.iteritems() if v is not None)
@@ -60,17 +60,17 @@ class Poloniex:
 
         if 'private' == type:
             post_data = urlencode(params)
-            
+
             sign = hmac.new(self.Secret, post_data, hashlib.sha512).hexdigest()
             headers = {
                 'Sign': sign,
                 'Key': self.APIKey
             }
-            
+
             ret = urlopen(Request('https://poloniex.com/tradingApi', post_data, headers))
             # jsonRet = json.loads(ret.read())
             # return self.post_process(jsonRet)
-        
+
         if self.parseJson:
             return json.loads(ret.read().decode('utf-8'))
         else:
@@ -84,25 +84,25 @@ class Poloniex:
 
     def _public(self, command, params={}):
         params['command'] = command
-        
+
         return self.api('public', params)
 
     def returnTicker(self):
         return self._public("returnTicker")
- 
+
     def return24hVolume(self):
         return self._public("return24hVolume")
- 
+
     def returnOrderBook (self, currencyPair):
         return self._public("returnOrderBook", {'currencyPair': currencyPair})
- 
+
     def returnTradeHistory (self, currencyPair, start=None, end=None):
         return self._public("returnTradeHistory", {
             'currencyPair': currencyPair,
             'start': createTimeStamp(start),
             'end': createTimeStamp(end)
         })
- 
+
     def returnChartData (self, currencyPair, period=300, start=None, end=None):
         return self._public("returnChartData", {
             'currencyPair': currencyPair,
@@ -110,14 +110,14 @@ class Poloniex:
             'end': createTimeStamp(end),
             'period': period
         })
- 
- 
+
+
     # Returns all of your balances.
     # Outputs:
     # {"BTC":"0.59098578","LTC":"3.31117268", ... }
     def returnBalances(self):
         return self._private('returnBalances')
- 
+
     # Returns your open orders for a given market, specified by the "currencyPair" POST parameter, e.g. "BTC_XCP"
     # Inputs:
     # currencyPair  The currency pair e.g. "BTC_XCP"
@@ -129,8 +129,8 @@ class Poloniex:
     # total         Total value of order (price * quantity)
     def returnOpenOrders(self,currencyPair):
         return self._private('returnOpenOrders',{"currencyPair":currencyPair})
- 
- 
+
+
     # Returns your trade history for a given market, specified by the "currencyPair" POST parameter
     # Inputs:
     # currencyPair  The currency pair e.g. "BTC_XCP"
@@ -142,7 +142,7 @@ class Poloniex:
     # type          sell or buy
     def returnAccountTradeHistory(self,currencyPair):
         return self._private('returnTradeHistory',{"currencyPair":currencyPair})
- 
+
     # Places a buy order in a given market. Required POST parameters are "currencyPair", "rate", and "amount". If successful, the method will return the order number.
     # Inputs:
     # currencyPair  The curreny pair
@@ -152,7 +152,7 @@ class Poloniex:
     # orderNumber   The order number
     def buy(self,currencyPair,rate,amount):
         return self._private('buy',{"currencyPair":currencyPair,"rate":rate,"amount":amount})
- 
+
     # Places a sell order in a given market. Required POST parameters are "currencyPair", "rate", and "amount". If successful, the method will return the order number.
     # Inputs:
     # currencyPair  The curreny pair
@@ -162,16 +162,16 @@ class Poloniex:
     # orderNumber   The order number
     def sell(self,currencyPair,rate,amount):
         return self._private('sell',{"currencyPair":currencyPair,"rate":rate,"amount":amount})
- 
+
     # Cancels an order you have placed in a given market. Required POST parameters are "currencyPair" and "orderNumber".
     # Inputs:
     # currencyPair  The curreny pair
     # orderNumber   The order number to cancel
     # Outputs:
     # succes        1 or 0
-    def cancel(self,currencyPair,orderNumber):
-        return self._private('cancelOrder',{"currencyPair":currencyPair,"orderNumber":orderNumber})
- 
+    def cancel(self, orderNumber):
+        return self._private('cancelOrder', {"orderNumber":orderNumber})
+
     # Immediately places a withdrawal for a given currency, with no email confirmation. In order to use this method, the withdrawal privilege must be enabled for your API key. Required POST parameters are "currency", "amount", and "address". Sample output: {"response":"Withdrew 2398 NXT."}
     # Inputs:
     # currency      The currency to withdraw
